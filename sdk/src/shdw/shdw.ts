d3d2515fd77952ef05eee924521f39b8d22480dc
alex <aabinaei@gmail.com>
2022-10-28 16:37:49 -0700

    typed shadow-drive

--- sdk/src/shdw/shdw.ts
+++ sdk/src/shdw/shdw.ts
@@ -1,15 +1,20 @@
 import {web3} from "@project-serum/anchor";
 import {ShdwDrive} from "@shadow-drive/sdk";
 import {version} from "./config";
+import {Metadata, encodeMetadata} from "../metadata";
 
-export async function client(connection, uploader) {
+export async function client(connection: web3.Connection, uploader: any): Promise<ShdwDrive> {
     console.log("build shdw client with finalized commitment");
     // build connection with finalized commitment for initial account creation
     const finalizedConnection = new web3.Connection(connection.rpcEndpoint, "finalized");
     return await new ShdwDrive(finalizedConnection, uploader).init();
 }
 
-export async function provision(connection, uploader, file) {
+export async function provision(
+    connection: web3.Connection,
+    uploader: any,
+    file: File
+): Promise<{ drive: ShdwDrive, account: web3.PublicKey }> {
     // build drive client
     const drive = await client(connection, uploader);
     // create storage account
@@ -21,42 +26,37 @@ export async function provision(connection, uploader, file) {
     return {drive, account}
 }
 
-export async function markAsImmutable(drive, account) {
+export async function markAsImmutable(drive: ShdwDrive, account: web3.PublicKey): Promise<void> {
     console.log("mark account as immutable");
     // time out for 1 second to give RPC time to resolve account
     await new Promise(r => setTimeout(r, 1000));
     await drive.makeStorageImmutable(account, version);
 }
 
-export async function uploadFile(file, drive, account) {
+export async function uploadFile(file: File, drive: ShdwDrive, account: web3.PublicKey): Promise<void> {
     console.log("upload file to shdw drive");
     await drive.uploadFile(account, file, version);
 }
 
-export async function editMetaData(file, drive, account, oldMetaData, newTitle) {
+export async function editMetaData(
+    drive: ShdwDrive,
+    account: web3.PublicKey,
+    oldMetaData: Metadata,
+    newTitle: string
+): Promise<void> {
     console.log("edit metadata on shdw drive");
     const url = buildUrl(account) + "meta.json";
-    const newMetaData = buildMetaData(oldMetaData.key, oldMetaData.lit, newTitle);
-    await drive.editFile(account, url, newMetaData, version);
-}
-
-export function buildMetaData(key, litArgs, title) {
-    const meta = {
-        key: key,
-        lit: litArgs,
-        title: title
+    const newMetadata = {
+        key: oldMetaData.key,
+        lit: oldMetaData.lit,
+        title: newTitle
     }
-    const json = JSON.stringify(meta);
-    const textEncoder = new TextEncoder();
-    const bytes = textEncoder.encode(json);
-    const blob = new Blob([bytes], {
-        type: "application/json;charset=utf-8"
-    });
-    return new File([blob], "meta.json");
+    const encoded = encodeMetadata(newMetadata);
+    await drive.editFile(account, url, encoded, version);
 }
 
 const URL_PREFIX = "https://shdw-drive.genesysgo.net/";
 
-function buildUrl(shadowAccount) {
+function buildUrl(shadowAccount: web3.PublicKey) {
     return (URL_PREFIX + shadowAccount.toString() + "/")
 }
